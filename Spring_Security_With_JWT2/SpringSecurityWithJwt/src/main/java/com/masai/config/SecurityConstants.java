b8eedd030208ad2e0e7458663998ff3ebe6c10b8
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2024-01-03 00:38:56 +0530

    practice spring security and review

--- Spring_Security_With_JWT2/SpringSecurityWithJwt/src/main/java/com/masai/config/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.masai.config;
-
-public interface SecurityConstants {
-
-	public static final String JWT_KEY ="secretsfhsfjhdkjngdfjkgfgjdlkfjsdkfjsd";
-	public static final String JWT_HEADER = "Authorization";
-	
-	
-}
