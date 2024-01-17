c8f8a710f660517e3661ca7fc8547bae8f7bb70a
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2024-01-18 00:36:28 +0530

    practice spring security and JWT

---
+++ Spring_Security_With_JWT2/SpringSecurityWithJwt/src/main/java/com/masai/service/CustomerServiceImpl.java
@@ -0,0 +1,44 @@
+package com.masai.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masai.exception.CustomerException;
+import com.masai.model.Customer;
+import com.masai.repository.CustomerRepository;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Override
+	public Customer registerCustomer(Customer customer) throws CustomerException {
+		
+		return customerRepository.save(customer);
+		
+		
+	}
+
+	@Override
+	public Customer getCustomerDetailsByEmail(String email)throws CustomerException {
+		
+		return customerRepository.findByEmail(email).orElseThrow(() -> new CustomerException("Customer Not found with Email: "+email));
+	}
+
+	@Override
+	public List<Customer> getAllCustomerDetails()throws CustomerException {
+		
+		List<Customer> customers= customerRepository.findAll();
+		
+		if(customers.isEmpty())
+			throw new CustomerException("No Customer find");
+		
+		return customers;
+		
+	}
+
+}
